c1c53d5efdb80a8c97beea1ef568fea804d4bad7
kenthua <pitchdarkice@gmail.com>
2015-08-06 13:26:02 -0700

    added ansible install to prep script

--- vagrant/multihost/box/prep_box.sh
+++ vagrant/multihost/box/prep_box.sh
@@ -23,6 +23,12 @@ yum -y install docker
 # need this back for nmcli for vagrant/virtualbox
 yum -y install NetworkManager
 
+# install ansible
+yum -y install \
+    http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
+sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+yum -y --enablerepo=epel install ansible
+
 # Modify sshd config
 sed -i -e '/^#UseDNS/a UseDNS no' /etc/ssh/sshd_config
 
