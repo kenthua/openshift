491b10700b9ae2f14b479e1c6e0d6b7d5c2044f3
khua <khua@redhat.com>
2015-11-30 12:31:09 -0800

    added multitenant commands and clean up

--- vagrant/multihost/ose3-master/post_install.sh
+++ vagrant/multihost/ose3-master/post_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# label the nodes because the ansible installer script doesn't work
-#oc label --overwrite node ose3-master.example.com region=infra zone=default
-#oc label --overwrite node ose3-node1.example.com region=primary zone=east
-#oc label --overwrite node ose3-node2.example.com region=primary zone=west
-
 # make the master node schedulable
 oadm manage-node ose-master.example.com --schedulable=true
 
@@ -66,18 +61,6 @@ oadm router router --replicas=1 \
     --stats-password='redhat' \
     --service-account=router
 
-# add routing config so new projects wil leverage specified subdomain 
-# not needed if ansible/hosts has this config already
-#echo "routingConfig:" >> /etc/openshift/master/master-config.yaml
-#echo "  subdomain: cloudapps.example.com" >> /etc/openshift/master/master-config.yaml
-
-# setup so that openshift doesn't authenticate passwords (any password is fine) - moved to ansible/hosts
-#sed -i -e "s/- name: deny_all/- name: anypassword/" /etc/openshift/master/master-config.yaml
-#sed -i -e "s/kind: DenyAllPasswordIdentityProvider/kind: AllowAllPasswordIdentityProvider/" /etc/openshift/master/master-config.yaml
-
-# restart openshift-master from all the config changes above
-#systemctl restart openshift-master
-
 # allow external docker images (Dockerfile) with USER requirements to run
 #oc get scc restricted -o yaml > scc-restricted.yaml
 #sed -i -e "s/type: MustRunAsRange/type: RunAsAny/" scc-restricted.yaml
@@ -86,4 +69,26 @@ oadm router router --replicas=1 \
 # oclogin script example
 #oc login https://ose-master.example.com:8443 --certificate-authority=/etc/origin/master/ca.crt -u alice -p anything
 
+# enable multitenant SDN
+#ssh root@ose-master.example.com 'sed -i -e "s/openshift-ovs-subnet/openshift-ovs-multitenant/" /etc/origin/master/master-config.yaml'
+#ssh root@ose-master.example.com 'sed -i -e "s/openshift-ovs-subnet/openshift-ovs-multitenant/" /etc/origin/node/node-config.yaml'
+#ssh root@ose-node1.example.com 'sed -i -e "s/openshift-ovs-subnet/openshift-ovs-multitenant/" /etc/origin/node/node-config.yaml'
+#ssh root@ose-node2.example.com 'sed -i -e "s/openshift-ovs-subnet/openshift-ovs-multitenant/" /etc/origin/node/node-config.yaml'
+# can't just restart service, need to **REBOOT** each server for networking to properly function
+# this seems to be the way until better instructions are provided
+##ssh root@ose-master.example.com 'systemctl restart atomic-openshift-master'
+##ssh root@ose-master.example.com 'systemctl restart atomic-openshift-node'
+##ssh root@ose-node1.example.com 'systemctl restart atomic-openshift-node'
+##ssh root@ose-node2.example.com 'systemctl restart atomic-openshift-node'
+# once enabled to join pod networks 'oadm pod-network join-projects --to=<project 1> <project 2>'
+# to make a project network global  'oadm pod-network make projects-global <project1> <project2>'
 
+
+
+# MOVED TO ansible/hosts add routing config so new projects wil leverage specified subdomain 
+##echo "routingConfig:" >> /etc/openshift/master/master-config.yaml
+##echo "  subdomain: cloudapps.example.com" >> /etc/openshift/master/master-config.yaml
+
+# MOVED TO ansible/hosts setup so that openshift doesn't authenticate passwords (any password is fine)
+##sed -i -e "s/- name: deny_all/- name: anypassword/" /etc/openshift/master/master-config.yaml
+##sed -i -e "s/kind: DenyAllPasswordIdentityProvider/kind: AllowAllPasswordIdentityProvider/" /etc/openshift/master/master-config.yaml
