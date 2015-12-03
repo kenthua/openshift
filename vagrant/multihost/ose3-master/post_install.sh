5e9594f3cf76b5d856cd3888b9ab679433cb2da9
khua <khua@redhat.com>
2015-12-02 16:39:54 -0800

    externalized additional functions to new script

--- vagrant/multihost/ose3-master/post_install.sh
+++ vagrant/multihost/ose3-master/post_install.sh
@@ -61,30 +61,6 @@ oadm router router --replicas=1 \
     --stats-password='redhat' \
     --service-account=router
 
-# allow external docker images (Dockerfile) with USER requirements to run
-#oc get scc restricted -o yaml > scc-restricted.yaml
-#sed -i -e "s/type: MustRunAsRange/type: RunAsAny/" scc-restricted.yaml
-#oc replace -f scc-restricted.yaml
-
-# oclogin script example
-#oc login https://ose-master.example.com:8443 --certificate-authority=/etc/origin/master/ca.crt -u alice -p anything
-
-# enable multitenant SDN
-#ssh root@ose-master.example.com 'sed -i -e "s/openshift-ovs-subnet/openshift-ovs-multitenant/" /etc/origin/master/master-config.yaml'
-#ssh root@ose-master.example.com 'sed -i -e "s/openshift-ovs-subnet/openshift-ovs-multitenant/" /etc/origin/node/node-config.yaml'
-#ssh root@ose-node1.example.com 'sed -i -e "s/openshift-ovs-subnet/openshift-ovs-multitenant/" /etc/origin/node/node-config.yaml'
-#ssh root@ose-node2.example.com 'sed -i -e "s/openshift-ovs-subnet/openshift-ovs-multitenant/" /etc/origin/node/node-config.yaml'
-# can't just restart service, need to **REBOOT** each server for networking to properly function
-# this seems to be the way until better instructions are provided
-##ssh root@ose-master.example.com 'systemctl restart atomic-openshift-master'
-##ssh root@ose-master.example.com 'systemctl restart atomic-openshift-node'
-##ssh root@ose-node1.example.com 'systemctl restart atomic-openshift-node'
-##ssh root@ose-node2.example.com 'systemctl restart atomic-openshift-node'
-# once enabled to join pod networks 'oadm pod-network join-projects --to=<project 1> <project 2>'
-# to make a project network global  'oadm pod-network make projects-global <project1> <project2>'
-
-
-
 # MOVED TO ansible/hosts add routing config so new projects wil leverage specified subdomain 
 ##echo "routingConfig:" >> /etc/openshift/master/master-config.yaml
 ##echo "  subdomain: cloudapps.example.com" >> /etc/openshift/master/master-config.yaml
