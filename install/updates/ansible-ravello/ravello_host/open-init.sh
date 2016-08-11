d983ba223c85ebc86b84cddc27dcf74e01bbef5d
khua <khua@redhat.com>
2016-08-11 07:26:54 -0700

    fixes for rhpds prep

--- install/updates/ansible-ravello/ravello_host/open-init.sh
+++ install/updates/ansible-ravello/ravello_host/open-init.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 export LOGFILE="/tmp/update.log"
+export STARTDATE=`date`;
+cat << EOF > /etc/motd
+###############################################################################
+###############################################################################
+###############################################################################
+Environment Provisioning In Progress : ${STARTDATE}
+DO NOT USE THIS ENVIRONMENT AT THIS POINT
+DISCONNECT AND TRY AGAIN 10 MINUTES FROM THE TIME ABOVE
+###############################################################################
+###############################################################################
+If you want, you can check out the status of the installer by using:
+tail -f /tmp/update.log
+###############################################################################
+
+EOF
+
 hostname -s| awk -F"-" '{print $1}' 2>&1 | tee $LOGFILE
 guid=`cat /usr/local/bin/configureIPA.sh | awk '{print $9}' | cut -c 19-22`
 echo "GUID: $guid" 2>&1 | tee $LOGFILE
@@ -15,7 +31,7 @@ systemctl restart named
 
 cd /root
 echo "Clone openshift" 2>&1 | tee -a $LOGFILE
-git clone https://github.com/kenthua/openshift
+git clone https://github.com/kenthua/openshift -b rhpds-cicd
 cd /root/openshift/install/updates/ansible-ravello
 echo "Change variables" 2>&1 | tee -a $LOGFILE
 sed -i "s/^ose_wildcard: apps/ose_wildcard: cloudapps/g" vars.yml
@@ -25,8 +41,29 @@ sed -i "s/^guid: /guid: -$guid/g" vars.yml
 sed -i "s/^ci_enabled: false/ci_enabled: true/g" vars.yml
 echo "[openshift_master]" > hosts
 echo "10.0.0.2" >> hosts
+echo "" >> hosts
+echo "[vms]" >> hosts
+echo "10.0.0.3" >> hosts
+echo "10.0.0.4" >> hosts
+echo "10.0.0.5" >> hosts
+echo "10.0.0.100" >> hosts
 echo "Run playbook" 2>&1 | tee -a $LOGFILE
 ansible-playbook -i hosts ravello_ocp_master.yml 2>&1 | tee -a $LOGFILE
-echo "Complete" 2>&1 | tee -a $LOGFILE
+echo "###############################################################################
+"
+echo "Provisioning Complete" 2>&1 | tee -a $LOGFILE
+echo "###############################################################################
+"
 
 
+export DATE=`date`;
+cat << EOF > /etc/motd
+###############################################################################
+Environment Provisioning Started      : ${STARTDATE}
+###############################################################################
+###############################################################################
+Environment Provisioning Is Completed : ${DATE}
+###############################################################################
+###############################################################################
+
+EOF
