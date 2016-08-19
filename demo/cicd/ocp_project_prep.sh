ddb3f6278862d4a41960963e76cee8e2fc4c68d3
khua <khua@redhat.com>
2016-08-19 14:31:51 -0700

    support for being able to run both scenarios

--- demo/cicd/ocp_project_prep.sh
+++ demo/cicd/ocp_project_prep.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
-oc delete project dev test prod
+SRC_DEV=src-dev
+SRC_TEST=src-test
+SRC_PROD=src-prod
+
+BIN_DEV=bin-dev
+BIN_TEST=bin-test
+BIN_PROD=bin-prod
+
+OSE_CI_PROJECT=ci
+
+SRC_PROJECTS="$SRC_DEV $SRC_TEST $SRC_PROD"
+BIN_PROJECTS="$BIN_DEV $BIN_TEST $BIN_PROD"
+PROJECTS="$SRC_PROJECTS $BIN_PROJECTS"
+
+for PROJECT in $PROJECTS;
+    do
+        oc delete $PROJECT
+    done
 
 sleep 20
 
-oc new-project dev
-oc new-project test
-oc new-project prod
-
+for PROJECT in $PROJECTS;
+    do
+        oc new-project $PROJECT
+    done
+    
 # no longer recommended
 # https://docs.openshift.com/enterprise/3.2/dev_guide/managing_images.html - Importing Images across projects - edit access (better?) or system:image-puller
-oc policy add-role-to-user edit system:serviceaccount:test:default -n dev
-oc policy add-role-to-user edit system:serviceaccount:uat:default -n dev
-oc policy add-role-to-user edit system:serviceaccount:prod:default -n dev
 
-oc policy add-role-to-user edit system:serviceaccount:ci:default -n dev
-oc policy add-role-to-user edit system:serviceaccount:ci:default -n test
-oc policy add-role-to-user edit system:serviceaccount:ci:default -n prod
+for PROJECT in $SRC_PROJECTS;
+    do
+        oc policy add-role-to-user edit system:serviceaccount:$PROJECT:default -n $SRC_DEV
+        oc policy add-role-to-user edit system:serviceaccount:$OSE_CI_PROJECT:default -n $PROJECT
+    done
 
+for PROJECT in $SRC_PROJECTS;
+    do
+        oc policy add-role-to-user edit system:serviceaccount:$PROJECT:default -n $BIN_DEV
+        oc policy add-role-to-user edit system:serviceaccount:$OSE_CI_PROJECT:default -n $PROJECT
+    done
