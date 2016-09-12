310618c3c056cb09612e6c1ccdaef6ffc49a2b10
khua <khua@redhat.com>
2016-09-12 12:31:44 -0700

    added support for automatically filling in guid

--- demo/cicd/configure.sh
+++ demo/cicd/configure.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+export GUID=`hostname -s | cut  -c 8-11`
+sed -i -e "s/GUID/$GUID/" vars.yaml
 ansible-playbook -i "localhost," init.yaml
 ansible-playbook -i "localhost," ocp_project_prep.yaml
 
